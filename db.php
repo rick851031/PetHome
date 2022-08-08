59802dae1655a17ca908cecc8eb665f5b8151ebd
rick851031 <110797649+rick851031@users.noreply.github.com>
2022-08-08 11:47:31 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?
 
  $db_host = "sql103.byethost14.com";
- $db_username = "b14_31510604";
- $db_password = "rick1031";
- $db_name = "b14_31510604_PetHomeNew"; 
+ $db_username = "";
+ $db_password = "";
+ $db_name = ""; 
 
  try{
 	 $conn = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8",$db_username,$db_password);
